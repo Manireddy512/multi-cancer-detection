258237e4950da4a74e125b5d5a969f5c3402f287
Manireddy512 <mani8309316076@gmail.com>
2026-03-09 12:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def index():
             prediction = "No Cancer Detected"
 
     return render_template("index.html", prediction=prediction)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
